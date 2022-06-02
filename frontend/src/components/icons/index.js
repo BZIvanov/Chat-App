b729ecf8c35404ec40029ffc549e20acf7f37ac9
BZIvanov <biser.z.ivanov@gmail.com>
2022-06-02 10:35:18 +0300

    icons folder

---
+++ frontend/src/components/icons/index.js
@@ -0,0 +1,5 @@
+import { ExitToApp, Menu, Send } from '@mui/icons-material';
+
+export const ExitToAppIcon = (props) => <ExitToApp {...props} />;
+export const MenuIcon = (props) => <Menu {...props} />;
+export const SendIcon = (props) => <Send {...props} />;
